6edd5cfc453b9b86cd31a9e3bb3ee0eca2e6df4d
Unknown <ajax66@outlook.fr>
2018-07-06 04:08:11 +0200

    uyguy

--- dnsrecord/formatJsonAddrIp.py
+++ dnsrecord/formatJsonAddrIp.py
@@ -54,7 +54,7 @@ source = "DnsRecord - Obtenir adresse Ip"
 if "A Records" in a:
 	for element in a["A Records"]:
 		listJsonData.append(CreateJsonNode(element, source, "L\'adresse IpV4 du nom de domaine", "Adresse IpV4", [], True))
-if "AAAA Records" != "":
+if len(a["AAAA Records"]) != 0:
 	listJsonData.append(CreateJsonNode(a["AAAA Records"], source, "L\'adresse IpV6 du nom de domaine", "Adresse IpV6", [], True))
 
 # The json result
